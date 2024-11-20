8889e237c5f7aebc83f31bf77374dff9132912a6
luban <3313094521@qq.com>
2024-11-20 15:10:50 +0800

    feat: 集成shiro 1

--- light-web/src/main/java/com/light/web/common/shiro/credentials/JwtTokenCredentialsMatcher.java
+++ light-web/src/main/java/com/light/web/common/shiro/credentials/JwtTokenCredentialsMatcher.java
@@ -4,7 +4,7 @@ import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.credential.CredentialsMatcher;
 
-import com.dzq.portal.web.common.shiro.token.JwtToken;
+import com.light.framework.mvc.filter.shiro.token.JwtToken;
 
 /**
  * 自定义本地的token凭证匹配器 这个方法很重要，决定了基于token验证的方案的成败 即：doGetAuthenticationInfo 执行的成败，如果失败了，结果会失败，内容未知（也可能没任何数据）
